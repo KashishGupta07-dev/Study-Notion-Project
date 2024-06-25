051f59f3a7ff8263801864192807b86f4fe02d32
KashishGupta07-dev <kashishgupta.y2k5@gmail.com>
2024-06-25 19:42:42 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.listen(PORT,()=>{
     console.log(`Server Started Successfully at port : ${PORT}`);
 });
 app.use(cors({
-	origin:"https://study-notion-frontend-8ovo.onrender.com/",
+	origin:"https://study-notion-frontend-omyjrxp66.vercel.app/",
 	credentials:true,
 }))
 app.use(fileUpload({
